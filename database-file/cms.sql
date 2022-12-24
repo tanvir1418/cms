1d9570d4eb9729dcc56ec539f5f8a365e7f703e3
Tanvir Ahmed <tanvirahmed1418@gmail.com>
2022-12-24 20:50:59 +0600

    Creating Database Relationships

--- database-file/cms.sql
+++ database-file/cms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 23, 2022 at 03:19 AM
+-- Generation Time: Dec 24, 2022 at 03:50 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -31,18 +31,75 @@ CREATE TABLE `article` (
   `id` int(11) NOT NULL,
   `title` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `published_at` datetime DEFAULT NULL
+  `published_at` datetime DEFAULT NULL,
+  `image_file` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `title`, `content`, `published_at`) VALUES
-(1, 'First posts', 'This is the first of many posts!', '2022-11-13 07:23:42'),
-(2, 'Another post', 'Yet another fascinating post...!', '2022-11-13 07:23:42'),
-(3, 'Read this!', 'You must read this now, it\'s essential reading!', '2022-11-13 07:24:39'),
-(4, 'The latest news', 'Here\'s the latest news, read it now.', '2022-11-13 07:24:39');
+INSERT INTO `article` (`id`, `title`, `content`, `published_at`, `image_file`) VALUES
+(1, 'First post', 'This is the first of many posts!', '2022-11-13 07:23:42', NULL),
+(2, 'Another post', 'Yet another fascinating post...!', '2022-11-13 07:23:42', NULL),
+(3, 'Read this!', 'You must read this now, it\'s essential reading!', '2022-11-13 07:24:39', NULL),
+(4, 'The latest news', 'Here\'s the latest news, read it now.', '2022-11-13 07:24:39', NULL),
+(5, 'An updated article', 'The latest post from me with another edit', '2022-12-24 19:40:00', NULL),
+(6, 'A new article', 'Some content', '2022-12-25 19:40:00', NULL),
+(7, 'Another new article', 'Some content', '2022-12-26 19:41:00', NULL),
+(8, 'The latest news', 'Latest news details', '2022-12-25 19:41:00', NULL),
+(9, 'Article of the day', 'Read this content', '2022-12-24 19:46:00', NULL),
+(10, 'Very interesting article', 'Something to read', '2022-12-24 19:46:00', NULL),
+(11, 'More things to discuss', 'Let\'s talk about this', '2022-12-22 19:47:00', NULL),
+(12, 'An example post', 'With some extra content', '2023-01-03 19:47:00', NULL),
+(13, 'Posting is fun', 'And interesting', '2022-12-24 19:48:00', NULL),
+(14, 'Let\'s discuss important item', 'And then talk some more', '2022-12-30 19:48:00', NULL),
+(15, 'What do you think?', 'Of this important topic', '2022-12-24 19:49:00', NULL),
+(16, 'News of the day', 'To read', '2022-12-31 19:49:00', NULL),
+(17, 'An update to the most recent article', 'With more information', '2023-01-06 19:50:00', NULL),
+(18, 'Some more news', 'This is really interesting', '2022-12-24 19:53:30', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_category`
+--
+
+CREATE TABLE `article_category` (
+  `article_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `article_category`
+--
+
+INSERT INTO `article_category` (`article_id`, `category_id`) VALUES
+(1, 1),
+(1, 3),
+(2, 4),
+(12, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `name`) VALUES
+(3, 'education'),
+(1, 'news'),
+(4, 'products'),
+(2, 'technology');
 
 -- --------------------------------------------------------
 
@@ -75,6 +132,20 @@ ALTER TABLE `article`
   ADD KEY `title` (`title`);
 
 --
+-- Indexes for table `article_category`
+--
+ALTER TABLE `article_category`
+  ADD PRIMARY KEY (`article_id`,`category_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `name` (`name`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -89,13 +160,30 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `article_category`
+--
+ALTER TABLE `article_category`
+  ADD CONSTRAINT `article_category_ibfk_1` FOREIGN KEY (`article_id`) REFERENCES `article` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `article_category_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
